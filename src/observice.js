47a14e2ea4fefd058680494cf37040c9051326df
Jared Tilston <jaredtilston@jareds-imac.sportssupltd.local>
2016-02-09 09:58:40 +0000

    Initial commit

---
+++ src/observice.js
@@ -0,0 +1,7 @@
+var uniqueRandomArray = require('unique-random-array');
+var countries = require('./countries.json');
+
+module.exports = {
+	all: countries,
+	random: uniqueRandomArray(countries).name
+};
